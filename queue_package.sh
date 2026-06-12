87f5257950fff3bd0b9f11ab13188b0a1178ca90
kirmola <kirmolaa@gmail.com>
2026-06-12 16:47:38 +0530

    500 packages for now

--- queue_package.sh
+++ queue_package.sh
@@ -1,11 +1,9 @@
-start=111
-count=40
+start=451
+count=50
 
 sed -n "${start},$((start+count-1))p" /home/kirmola/Downloads/appid.txt | while read -r pkg; do
 
   gh workflow run apkdl.yaml \
     --ref main \
     -f package_name="$pkg"
-
-  sleep 1
 done
